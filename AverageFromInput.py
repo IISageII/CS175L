a0ea593a6b6a1eda3188e4b8019b5c677ad93078
IISageII <146123687+IISageII@users.noreply.github.com>
2024-03-19 11:45:37 -0400

    AverageFromInput
    Chapter 6 Average From Input File with Exception Handling

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -3,18 +3,25 @@
 #AverageFromInput
 
 def main():
-    data = open('numbers.txt', 'r')
+    import sys
+    try:
+        data = open('numbers.txt', 'r')
+    except IOError:
+        sys.exit('File not found: numbers.txt - exiting')
     num = 0
     total = 0
     for line in data:
-        num +=1
-        content = float(line)
-        total += float(content)
-        print(f'I read in {num} number(s) Current number is:', end='')
-        if content >= 0:
-            print(f'    {content:.2f} Total is:    {total:.2f}')
-        else:
-            print(f'   {content:.2f} Total is:   {total:.2f}')
+        try:
+            content = float(line)
+            total += float(content)
+            num +=1
+            print(f'I read in {num} number(s) Current number is:', end='')
+            if content >= 0:
+                print(f'    {content:.2f} Total is:    {total:.2f}')
+            else:
+                print(f'   {content:.2f} Total is:   {total:.2f}')
+        except ValueError:
+            print(f'Bad data: {line.strip()} skipping')
     data.close()
     average = float(total / num)
     print(f'Average: {average:.1f}')
